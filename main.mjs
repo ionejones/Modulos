e9a566a7cc643cefbb5e78f2c4e6664d361032fd
ione <ionejones@gmail.com>
2022-06-28 23:07:32 -0300

    Exemplo do uso de modulos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade('Ione', 'Salvador'));
+console.log(mostraIdade('Ione','47'));
+console.log(mostraHobby('Ione','tocar piano'));
